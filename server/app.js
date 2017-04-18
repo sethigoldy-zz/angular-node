b2b7074a7e4fd0ac560b688534001f57056ca12e
pnkjjsr <pnkj_jsr@yahoo.co.in>
2017-04-18 17:44:21 +0530

    Master: express connected.

--- server/app.js
+++ server/app.js
@@ -4,43 +4,59 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var index = require('./routes/index');
 var users = require('./routes/users');
-
 var app = express();
 
-// view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, '../client/app', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+if ('development' == app.get('env')) {
+    // This will change in production since we'll be using the dist folder
+    app.use(express.static(path.join(__dirname, '../client/.tmp')));
+
+    // This covers serving up the index page
+    app.use(express.static(path.join(__dirname, '../client/app')));
+
+    // This is the new way to handle errors in Express 4. not errorHandler().
+    // For more about error-first best practices see http://fredkschott.com/post/2014/03/understanding-error-first-callbacks-in-node-js/
+    app.use(function (err, req, res, next) {
+        console.error(err.stack);
+        res.send(500, 'Something broke!');
+    });
+}
+
+/**
+ * Production Settings
+ */
+if ('production' == app.get('env')) {
+    app.use(express.static(path.join(__dirname, '/dist')));
+}
+
+/* Add this to fire the server */
+var server = app.listen(3000, function () {
+    console.log('Listening on port %d', server.address().port);
+});
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
